7e53763634e90a1ccf5e664ee25ba9a72728fdfd
Maksymilian Szymański <maksymilianszymanski2@gmail.com>
2025-03-28 15:23:33 +0100

    save

--- src/app/lib/actions.js
+++ src/app/lib/actions.js
@@ -24,11 +24,11 @@ export async function sendEmail(formData) {
             to: process.env.SMTP_USER,
             subject: `Nowa wiadomość z formularza kontaktowego od ${name}`,
             text: `Imię: ${name}\nTelefon: ${phone}\n\nWiadomość:\n${message}`,
-            html: `<div style="max-width: 600px; margin: 20px auto; padding: 20px; border: 1px solid #ddd; border-radius: 8px; font-family: Arial, sans-serif; background-color: #f9f9f9; color: #333;">
+            html: `<div style="padding: 20px;"><div style="max-width: 600px; margin: 20px auto; padding: 20px; border: 1px solid #ddd; border-radius: 8px; font-family: Arial, sans-serif; background-color: #f9f9f9; color: #333;">
         <p><strong>Imię:</strong> ${name}</p>
         <p><strong>Telefon:</strong> ${phone}</p>
         <p style="margin-top: 20px;"><strong>Wiadomość:</strong><br>${message}</p>
-    </div>`,
+    </div></div>`,
         })
 
         return { status: 'success' }
